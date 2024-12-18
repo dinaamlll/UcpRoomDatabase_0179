2c86a64c3701bf51f9e3c6741a4a5f4bd6a29bde
dinaamlll <dinamaliaa17@gmail.com>
2024-12-18 19:33:25 +0700

    Menambahkan LocalRepositoryDsn untuk operasi insert dan read pada Dosen

--- app/src/main/java/com/example/ucp2/repository/LocalRepositoryDsn.kt
+++ app/src/main/java/com/example/ucp2/repository/LocalRepositoryDsn.kt
@@ -2,11 +2,19 @@ package com.example.ucp2.repository
 
 import com.example.ucp2.data.dao.DosenDao
 import com.example.ucp2.data.entity.Dosen
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryDsn (
     private val dosenDao: DosenDao
 ) : RepositoryDsn {
     override suspend fun insertDsn(dosen: Dosen) {
         dosenDao.insertDosen(dosen)
+    }
+    override fun getAllDosen(): Flow<List<Dosen>> {
+        return dosenDao.getAllDosen()
+    }
+
+    override fun getDosen(nidn: String): Flow<Dosen> {
+        return dosenDao.getDosen(nidn)
     }
 }
