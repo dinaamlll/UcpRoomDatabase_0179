1f65e341148fcd2ed64b9277b8aa92ebd59b3a57
dinaamlll <dinamaliaa17@gmail.com>
2024-12-18 19:27:44 +0700

    Menambahkan operasi insert dan read di RepositoryDsn

--- app/src/main/java/com/example/ucp2/repository/RepositoryDsn.kt
+++ app/src/main/java/com/example/ucp2/repository/RepositoryDsn.kt
@@ -1,7 +1,10 @@
 package com.example.ucp2.repository
 
 import com.example.ucp2.data.entity.Dosen
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryDsn {
     suspend fun insertDsn(dosen: Dosen)
+    fun getAllDosen() : Flow<List<Dosen>>
+    fun getDosen(nim: String) : Flow<Dosen>
 }
