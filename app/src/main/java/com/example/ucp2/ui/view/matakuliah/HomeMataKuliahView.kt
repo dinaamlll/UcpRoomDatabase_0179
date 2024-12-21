2735a9e1d8f60e556412fb529dd3a3d82969624b
dinaamlll <dinamaliaa17@gmail.com>
2024-12-22 02:42:09 +0700

    membuat fungsi HomeMatakuliahViewdi HomeMataKuliahView.kt

--- app/src/main/java/com/example/ucp2/ui/view/matakuliah/HomeMataKuliahView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/matakuliah/HomeMataKuliahView.kt
@@ -9,14 +9,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -29,9 +34,51 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ucp2.data.entity.MataKuliah
+import com.example.ucp2.ui.costumwidget.CostumTopAppBar
+import com.example.ucp2.ui.viewmodel.PenyediaViewModel
 import com.example.ucp2.ui.viewmodel.dosen.HomeUiState
 import kotlinx.coroutines.launch
 
+@Composable
+fun HomeMatakuliahView(
+    viewModel: HomeMatakuliahViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onAddMatakuliah: () -> Unit = {},
+    onDetailClick: (String) -> Unit = {},
+    modifier: Modifier = Modifier
+) {
+    Scaffold(
+        topBar = {
+            CostumTopAppBar(
+                judul = "Daftar Mata Kuliah",
+                showBackButton = false,
+                onBack = {},
+                modifier = modifier
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onAddMatakuliah,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Tambah Mata Kuliah",
+                )
+            }
+        }
+    ) { innerPadding ->
+        val homeUiState by viewModel.homeUiState.collectAsState()
+
+        BodyHomeMatakuliahView(
+            homeUiState = homeUiState,
+            onClick = {
+                onDetailClick(it)
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
 @Composable
 fun BodyHomeMatakuliahView(
     homeUiState: HomeUiState,
