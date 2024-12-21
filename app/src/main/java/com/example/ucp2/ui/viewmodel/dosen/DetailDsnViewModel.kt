53336963ea3eae75e58bf5888577c19c35b95e67
dinaamlll <dinamaliaa17@gmail.com>
2024-12-22 01:51:20 +0700

    membuat class DetailDsnViewModel

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/dosen/DetailDsnViewModel.kt
@@ -0,0 +1,54 @@
+package com.example.ucp2.ui.viewmodel.dosen
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2.data.entity.Dosen
+import com.example.ucp2.repository.RepositoryDsn
+import com.example.ucp2.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+class DetailDsnViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryDsn: RepositoryDsn,
+) : ViewModel() {
+    private val _nidn: String = checkNotNull(savedStateHandle[DestinasiDetail.NIDN])
+
+    val detailUiState: StateFlow<DetailUiState> = repositoryDsn.getDosen(_nidn)
+        .filterNotNull()
+        .map {
+            DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(DetailUiState(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoading = true,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan",
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailUiState(
+                isLoading = true,
+            ),
+        )
+
+
