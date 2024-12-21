b5f52930a6b7ae151149d13e337ef3528efb9f38
dinaamlll <dinamaliaa17@gmail.com>
2024-12-22 02:39:38 +0700

    membuat fungsi BodyHomeMatakuliahView di HomeMataKuliahView.kt

--- app/src/main/java/com/example/ucp2/ui/view/matakuliah/HomeMataKuliahView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/matakuliah/HomeMataKuliahView.kt
@@ -1,9 +1,11 @@
 package com.example.ucp2.ui.view.matakuliah
 
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -12,17 +14,82 @@ import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ucp2.data.entity.MataKuliah
+import com.example.ucp2.ui.viewmodel.dosen.HomeUiState
+import kotlinx.coroutines.launch
 
+@Composable
+fun BodyHomeMatakuliahView(
+    homeUiState: HomeUiState,
+    onClick: (String) -> Unit = {},
+    modifier: Modifier = Modifier
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+
+    when {
+        homeUiState.isLoading -> {
+            // Menampilkan indikator loading
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+
+        homeUiState.isError -> {
+            // Menampilkan pesan error
+            LaunchedEffect(homeUiState.errorMessage) {
+                homeUiState.errorMessage?.let { message ->
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(message) // Tampilkan Snackbar
+                    }
+                }
+            }
+        }
+
+        homeUiState.listMatakuliah.isEmpty() -> {
+            // Menampilkan pesan jika data kosong
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Tidak ada data mata kuliah",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+
+        else -> {
+            // Menampilkan daftar mata kuliah
+            ListMatakuliah(
+                listMatakuliah = homeUiState.listMatakuliah,
+                onClick = {
+                    onClick(it)
+                },
+                modifier = modifier
+            )
+        }
+    }
+}
 @Composable
 fun ListMatakuliah(
     listMatakuliah: List<MataKuliah>,
