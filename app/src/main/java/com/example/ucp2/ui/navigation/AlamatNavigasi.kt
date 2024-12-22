6f71411246c837171e489944af0a804ed2a77ee9
dinaamlll <dinamaliaa17@gmail.com>
2024-12-22 14:06:04 +0700

    Menambahkan DestinasiDosen dan DestinasiMatakuliah

--- app/src/main/java/com/example/ucp2/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/ucp2/ui/navigation/AlamatNavigasi.kt
@@ -7,20 +7,26 @@ interface AlamatNavigasi {
 object DestinasiHome : AlamatNavigasi {
     override val route = "home"
 }
+object DestinasiDosen : AlamatNavigasi {
+    override val route = "dosen"
+}
 object DestinasiDosenDetail : AlamatNavigasi {
     override val route = "dosendetail"
     const val NIDN = "nidn"
     val routesWithArg = "$route/{$NIDN}"
 }
+object DestinasiMatakuliah : AlamatNavigasi {
+    override val route = "matakuliah"
+}
 
-object DestinasiDetail : AlamatNavigasi {
-    override val route = "detail"
+object DestinasiMatakuliahDetail : AlamatNavigasi {
+    override val route = "matakuliahdetail"
     const val KODE = "kode"
     val routesWithArg = "$route/{$KODE}"
 }
 
-object DestinasiUpdate : AlamatNavigasi {
-    override val route = "update"
+object DestinasiMatakuliahUpdate : AlamatNavigasi {
+    override val route = "matakuliahupdate"
     const val KODE = "kode"
     val routesWithArg = "$route/{$KODE}"
 }
