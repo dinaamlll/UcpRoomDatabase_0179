bbf29e9f9c0f5c92955947f3c5be9d7a35aa9a12
dinaamlll <dinamaliaa17@gmail.com>
2024-12-22 13:54:50 +0700

    mengganti import dosen menjadi matakuliah

--- app/src/main/java/com/example/ucp2/ui/view/matakuliah/HomeMataKuliahView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/matakuliah/HomeMataKuliahView.kt
@@ -26,6 +26,8 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
@@ -33,16 +35,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp2.data.entity.MataKuliah
 import com.example.ucp2.ui.costumwidget.CostumTopAppBar
 import com.example.ucp2.ui.viewmodel.PenyediaViewModel
-import com.example.ucp2.ui.viewmodel.dosen.HomeUiState
+import com.example.ucp2.ui.viewmodel.matakuliah.HomeUiState
+import com.example.ucp2.ui.viewmodel.matakuliah.HomeMatakuliahViewModel
 import kotlinx.coroutines.launch
 
 @Composable
 fun HomeMatakuliahView(
     viewModel: HomeMatakuliahViewModel = viewModel(factory = PenyediaViewModel.Factory),
     onAddMatakuliah: () -> Unit = {},
+    onBack:()->Unit,
     onDetailClick: (String) -> Unit = {},
     modifier: Modifier = Modifier
 ) {
