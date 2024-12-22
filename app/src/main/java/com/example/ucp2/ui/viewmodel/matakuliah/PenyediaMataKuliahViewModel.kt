6d94d4f1df0895fab2188e8845fe873b3a9d0a62
dinaamlll <dinamaliaa17@gmail.com>
2024-12-22 13:40:51 +0700

    membuat object PenyediaViewModel

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/PenyediaMataKuliahViewModel.kt
@@ -0,0 +1,39 @@
+package com.example.ucp2.ui.viewmodel.matakuliah
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.ucp2.KrsApp
+
+object PenyediaViewModel{
+
+    val Factory = viewModelFactory {
+        initializer {
+            MatakuliahViewModel(
+                krsApp().containerApp.repositoryMataKuliah
+            )
+        }
+        initializer {
+            HomeMatakuliahViewModel(
+                krsApp().containerApp.repositoryMataKuliah
+            )
+        }
+        initializer {
+            DetailMatakuliahViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMataKuliah,
+            )
+        }
+        initializer {
+            UpdateMatakuliahViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMataKuliah,
+            )
+        }
+    }
+}
+
+fun CreationExtras.krsApp(): KrsApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KrsApp)
