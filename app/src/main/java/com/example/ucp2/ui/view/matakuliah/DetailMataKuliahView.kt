9ea98cc1ce67e190b4b598fa92f70159a8719683
dinaamlll <dinamaliaa17@gmail.com>
2024-12-23 12:53:51 +0700

    menambahkan delete

--- app/src/main/java/com/example/ucp2/ui/view/matakuliah/DetailMataKuliahView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/matakuliah/DetailMataKuliahView.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
@@ -16,9 +18,13 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -76,8 +82,10 @@ fun DetailMatakuliahView(
 @Composable
 fun BodyDetailMatakuliah(
     modifier: Modifier = Modifier,
-    detailUiState: DetailUiState = DetailUiState()
+    detailUiState: DetailUiState = DetailUiState(),
+    onDeleteClick: () -> Unit = {}
 ) {
+    var deleteConfirmationRequest by rememberSaveable { mutableStateOf(false) }
     when {
         detailUiState.isLoading -> {
             Box(
@@ -98,6 +106,25 @@ fun BodyDetailMatakuliah(
                     matakuliah = detailUiState.detailUiEvent.toMatakuliahEntity(),
                     modifier = Modifier
                 )
+                Spacer(modifier = Modifier.padding(8.dp))
+                Button(
+                    onClick = {
+                        deleteConfirmationRequest = true
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = "Delete")
+                }
+                if (deleteConfirmationRequest) {
+                    DeleteConfirmationDialog(
+                        onDeleteConfirm = {
+                            deleteConfirmationRequest = false
+                            onDeleteClick()
+                        },
+                        onDeleteCancel = { deleteConfirmationRequest = false },
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
             }
         }
 
@@ -138,6 +165,10 @@ fun ItemDetailMatakuliah(
             Spacer(modifier = Modifier.padding(4.dp))
             ComponentDetailMatakuliah(judul = "Semester", isinya = matakuliah.semester)
             Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailMatakuliah(judul = "Jenis Matakuliah", isinya = matakuliah.jenis)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailMatakuliah(judul = "Dosen Pengampu", isinya = matakuliah.dosenPengampu)
+            Spacer(modifier = Modifier.padding(4.dp))
         }
     }
 }
@@ -164,4 +195,23 @@ fun ComponentDetailMatakuliah(
             fontWeight = FontWeight.Bold
         )
     }
+}
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit, onDeleteCancel:() -> Unit, modifier: Modifier = Modifier
+){
+    AlertDialog(onDismissRequest = {},
+        title ={ Text("Delete Data") },
+        text ={ Text("Apakah anda yakin ingin menghapus data?") },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(text = "Cancel")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = "Yes")
+            }
+        })
 }
